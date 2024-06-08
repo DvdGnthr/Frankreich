1e7402122c164ebfcf55db60feac793bed7d36a3
yurrghurrter <yurrghurrt@gmail.com>
2024-06-08 22:31:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-const searchHighlighter = (elem, that) => {
-    let el = document.querySelector(elem);
-    const searchText = that.value.trim(); // Trimmen Sie die Sucheingabe, um Leerzeichen am Anfang und Ende zu entfernen
+const searchHighlighter = (containerSelector, inputElement) => {
+    const container = document.querySelector(containerSelector);
+    const searchText = inputElement.value.trim();
 
-    // Alle <mark> Tags im Element entfernen, um die Suche neu zu starten
-    el.querySelectorAll('mark').forEach(mark => {
-        mark.outerHTML = mark.innerHTML;
-    });
+    // Alle <mark> Tags im Container entfernen
+    const removeHighlights = (element) => {
+        const highlights = element.querySelectorAll('mark');
+        highlights.forEach((highlight) => {
+            const parent = highlight.parentNode;
+            parent.replaceChild(document.createTextNode(highlight.textContent), highlight);
+            parent.normalize();
+        });
+    };
 
-    if (searchText !== '') {
+    // Text durchsuchen und hervorheben
+    const highlightText = (element, regex) => {
+        element.childNodes.forEach((child) => {
+            if (child.nodeType === 3) { // Textknoten
+                const matches = child.nodeValue.match(regex);
+                if (matches) {
+                    const span = document.createElement('span');
+                    let lastIndex = 0;
+                    matches.forEach((match) => {
+                        const pos = child.nodeValue.indexOf(match, lastIndex);
+                        if (pos !== -1) {
+                            span.appendChild(document.createTextNode(child.nodeValue.substring(lastIndex, pos)));
+                            const mark = document.createElement('mark');
+                            mark.className = 'highlight';
+                            mark.appendChild(document.createTextNode(match));
+                            span.appendChild(mark);
+                            lastIndex = pos + match.length;
+                        }
+                    });
+                    span.appendChild(document.createTextNode(child.nodeValue.substring(lastIndex)));
+                    child.parentNode.replaceChild(span, child);
+                }
+            } else if (child.nodeType === 1 && child.childNodes && !/(script|style)/i.test(child.tagName)) {
+                highlightText(child, regex);
+            }
+        });
+    };
+
+    // Hervorhebung entfernen
+    removeHighlights(container);
+
+    if (searchText) {
         const regex = new RegExp(searchText, 'gi');
-        el.innerHTML = el.innerHTML.replace(regex, '<mark class="highlight">$&</mark>');
+        highlightText(container, regex);
     }
-
-    return el;
 };
